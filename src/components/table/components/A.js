6a3416e44b5f612753b3025c788843e3f6a98cb6
zhanghongwei <zhang123910@live.com>
2020-02-26 11:29:57 +0800

    feat: 添加table组件

--- src/components/table/components/A.js
+++ src/components/table/components/A.js
@@ -18,69 +18,51 @@ const AppList = props => {
 
   const { type, changeType, submit, reset } = search;
 
-  const advanceSearchForm = (
-    <div>
-      <Form>
-        <Row gutter={24}>
-          <Col span={8}>
-            <Form.Item label="name">
-              {getFieldDecorator('name')(<Input placeholder="name" />)}
-            </Form.Item>
-          </Col>
-          <Col span={8}>
-            <Form.Item label="email">
-              {getFieldDecorator('email')(<Input placeholder="email" />)}
-            </Form.Item>
-          </Col>
-          <Col span={8}>
-            <Form.Item label="phone">
-              {getFieldDecorator('phone')(<Input placeholder="phone" />)}
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row>
-          <Form.Item style={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <Button type="primary" onClick={submit}>
-              Search
-            </Button>
-            <Button onClick={reset} style={{ marginLeft: 16 }}>
-              Reset
-            </Button>
-            <Button type="link" onClick={changeType}>
-              Simple Search
-            </Button>
-          </Form.Item>
-        </Row>
-      </Form>
-    </div>
-  );
-
+  console.log('+=====>>type==>', type);
   const searchFrom = (
-    <div style={{ marginBottom: 16 }}>
-      <Form style={{ display: 'flex', justifyContent: 'flex-end' }}>
-        {getFieldDecorator('gender', {
-          initialValue: 'male',
-        })(
-          <Select style={{ width: 120, marginRight: 16 }} onChange={submit}>
-            <Option value="">all</Option>
-            <Option value="male">male</Option>
-            <Option value="female">female</Option>
-          </Select>,
-        )}
-        {getFieldDecorator('name')(
-          <Input.Search placeholder="enter name" style={{ width: 240 }} onSearch={submit} />,
-        )}
-        <Button type="link" onClick={changeType}>
-          Advanced Search
-        </Button>
-      </Form>
-    </div>
+    <>
+      <Row gutter={24}>
+        <Col span={8}>
+          <Form.Item label="name">
+            {getFieldDecorator('name')(<Input placeholder="name" />)}
+          </Form.Item>
+        </Col>
+        <Col span={8}>
+          <Form.Item label="email">
+            {getFieldDecorator('email')(<Input placeholder="email" />)}
+          </Form.Item>
+        </Col>
+        <Col span={8}>
+          <Form.Item label="phone">
+            {getFieldDecorator('phone')(<Input placeholder="phone" />)}
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row></Row>
+    </>
   );
 
   return (
     <div>
-      {type === 'simple' ? searchFrom : advanceSearchForm}
-      <Table columns={columns} rowKey="email" {...tableProps} />
+      {searchFrom ? (
+        <Form>
+          {searchFrom}
+          {
+            <Form.Item style={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <Button type="primary" onClick={submit}>
+                搜索
+              </Button>
+              <Button onClick={reset} style={{ marginLeft: 16 }}>
+                重置
+              </Button>
+              <Button type="link" onClick={changeType}>
+                Simple Search
+              </Button>
+            </Form.Item>
+          }
+        </Form>
+      ) : null}
+      <Table columns={columns} rowKey="eemaile" {...tableProps} />
     </div>
   );
 };

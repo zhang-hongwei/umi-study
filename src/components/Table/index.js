b8d29db46279ae60bc4750d38fac27480a99ada5
zhanghongwei <zhang123910@live.com>
2020-02-26 11:25:02 +0800

    feat: 添加组件

---
+++ src/components/Table/index.js
@@ -0,0 +1,7 @@
+import React, { useCallback, useMemo } from 'react';
+import { Table as AntTable, Form, Button } from 'antd';
+import { useAntdTable } from '@umijs/hooks';
+
+const Table = props => {
+  return <AntTable></AntTable>;
+};
